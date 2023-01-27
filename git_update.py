0f4cbb9d3ac2e22a4ed1469aad78b52282ee5665
mohsen-azimi <azimi.eng@gmail.com>
2023-01-26 23:38:06 -0800

    updated

--- git_update.py
+++ git_update.py
@@ -1,6 +1,6 @@
-import os
 import subprocess
 import socket
+
 
 def git_push():
     hostname = socket.gethostname()
@@ -8,9 +8,10 @@ def git_push():
         subprocess.run(["git", "add", "*.py"])
         subprocess.run(["git", "commit", "-m", "updated"])
         subprocess.run(["git", "push"])
-        print("Changes Pushed to GitHub")
+        print(f"Changes Pushed from {hostname} to GitHub")
     else:
         print("Code not running on Mohsen's Laptop, GitHub is not updated")
+
 
 if __name__ == '__main__':
     git_push()
